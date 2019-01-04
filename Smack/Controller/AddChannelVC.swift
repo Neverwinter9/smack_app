47050d016f09fbc262c3f0d2d5c09f5a043ef89d
Neverwinter9 <neverwinter99@gmail.com>
2019-01-03 18:44:30 -0800

    Created AddChannelVC

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,45 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Pavel Ponomarev on 1/3/19.
+//  Copyright © 2019 Pavel Ponomarev. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        
+        // Custom placeholders
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedString.Key.foregroundColor: smackPurplePlaceholder])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedString.Key.foregroundColor: smackPurplePlaceholder])
+        
+        // Tap gesture recognizer
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
